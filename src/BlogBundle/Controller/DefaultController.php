cfed5e6d5220ab95a94eb442becc4f771b988a9d
datata <danitt15@gmail.com>
2019-06-19 12:09:37 +0200

    Add BlogBundle & create bd & table

---
+++ src/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+   
+
+    /**
+     * @Route("/")
+     */
+    public function indexAction($nombre='Xavi')
+    {
+        return $this->render('@Blog/Default/index.html.twig',['nombre'=> $nombre]);
+    }
+
+    
+}
